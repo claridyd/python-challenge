be35c76f3debc6d748fa90be03d007f2f4394e63
Nisey C <denise.claridy@gmail.com>
2019-06-29 13:21:35 -0400

    changed location

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,8 +6,9 @@ import os
 import csv
 
 # Set path for file
-electiondata_csv = os.path.join("c:/", "DataVisual","UCFLM20190409DATA", "Homework","03-Python", 
- "Instructions", "PyPoll", "Resources", "election_data.csv")
+electiondata_csv = os.path.join("election_data.csv")
+
+# PyPoll\election_data.csv
 
 #List to store data
 VoterId = []
